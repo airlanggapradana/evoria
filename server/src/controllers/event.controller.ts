e19ea9cc80276bb8189553667a95ac018f367eee
Airlangga Pradana <ranggavfxartwork@gmail.com>
2025-11-12 15:32:54 +0700

    add authentication middleware; secure event and registration routes

--- server/src/controllers/event.controller.ts
+++ server/src/controllers/event.controller.ts
@@ -1,12 +1,13 @@
 import {Router} from "express"
 import {createEvent, deleteEventById, getAllEvents, getEventById, updateEventById} from "../services/event.service";
+import {authMiddleware} from "../middlewares/auth.middleware";
 
 const eventRouter = Router();
 
 eventRouter.get('/', getAllEvents)
-eventRouter.post('/', createEvent)
+eventRouter.post('/', authMiddleware, createEvent)
 eventRouter.get('/:id', getEventById)
 eventRouter.put('/:id', updateEventById)
-eventRouter.delete('/:id', deleteEventById)
+eventRouter.delete('/:id', authMiddleware, deleteEventById)
 
 export default eventRouter;
--- server/src/controllers/registration.controller.ts
+++ server/src/controllers/registration.controller.ts
@@ -5,11 +5,12 @@ import {
   midtransNotification,
   registerEvent
 } from "../services/registration.service";
+import {authMiddleware} from "../middlewares/auth.middleware";
 
 const registrationRouter = Router();
 
-registrationRouter.get('/:id', getRegistrationDetails)
-registrationRouter.post('/', registerEvent)
+registrationRouter.get('/:id', authMiddleware, getRegistrationDetails)
+registrationRouter.post('/', authMiddleware, registerEvent)
 registrationRouter.post('/check-in', checkInUser)
 registrationRouter.post('/midtrans-notification', midtransNotification)
 
