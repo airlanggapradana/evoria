fb5c3fce13ffda292768c139e8c094b47201e548
Airlangga Pradana <ranggavfxartwork@gmail.com>
2025-11-19 22:32:16 +0700

    fix: update cookie attributes for access and refresh tokens to support cross-site usage

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -40,13 +40,22 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
 
     res.cookie("access_token", accessToken, {
       httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      domain: env.FRONTEND_URL,
+      path: "/",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
     res.cookie("refresh_token", refreshToken, {
       httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      domain: env.FRONTEND_URL,
+      path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
+
 
     return res.status(200).json({
       message: "Login successful",
